9e2666bd3486b36beb1a480e1b106df44329850b
Trew Boisvert <trewboisvert@gmail.com>
2024-12-03 16:49:12 -0800

    Added basic replay feature

--- scratchwork.py
+++ scratchwork.py
@@ -39,15 +39,18 @@ test cases (2025)
 """
 import requests
 
-num_to_guess = requests.get('https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new').text.replace("\n", "")
-print(num_to_guess)
-print(type(num_to_guess))
+
 def game():
+    api_request = requests.get('https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new')
+    num_to_guess = api_request.text.replace("\n", "")
     num_guesses = 1
     history_guesses = []
+
     while num_guesses <= 10:
+        print("--------------GUESS HISTORY----------------")
         for guess in history_guesses:
             print(guess)
+        print("--------------------------------------------")
         print()
         print(f"Round {num_guesses} of 10")
         print()
@@ -63,30 +66,33 @@ def game():
         else:
             tally_correct_num_loc = 0
             tally_correct_num_no_loc = 0
-            #check for correct number and location
 
             for num in range(4):
                 if current_guess[num] == num_to_guess[num]:
-                    #update counter
                     tally_correct_num_loc += 1  
             copy_answer = list(num_to_guess)
-            print("copy answer", copy_answer)
-
-            #check correct guess wrong location   
+  
             for char in current_guess:
                 if char in copy_answer:
                     copy_answer.remove(char)
-                    #update counter
                     tally_correct_num_no_loc += 1
-            #check for all wrong (if both counters empty)
-            #give feedback
             if tally_correct_num_no_loc == 0:
                 feedback = "All wrong"
             else:
                 feedback = f"{tally_correct_num_no_loc} correct numbers and {tally_correct_num_loc} correct location"        
-            #add feedback to history
             history_guesses.append(f"Round {num_guesses} Guess: {current_guess} Feedback: {feedback}")
-            #add to num of guesses counter
             num_guesses += 1
+    print(f"The answer was {num_to_guess}")
 
-game()
+def play():
+    print("Welcome!")
+    while True:
+        print("Would you like to play a game of mastermind?")
+        play_game = input("y/n:   ")
+        if play_game == "y":
+            game()
+        else:
+            print("Alright, enjoy your day!")
+            break
+
+play()
